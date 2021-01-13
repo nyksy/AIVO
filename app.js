495063ba3179bcaab49fa7692f42c01c537bcd6a
nyksy <71010049+nyksy@users.noreply.github.com>
2021-01-13 15:38:17 +0200

    completed the scraper and imported it to the app.js

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const config = require('./utils/config')
 const express = require('express')
 const Discord = require('discord.js')
-const scraper = require('scraper')
+const scraper = require('./utils/scraper')
 
 const app = express()
 
+//Discord client
 const client = new Discord.Client();
+
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -13,11 +15,12 @@ client.on('ready', () => {
 
 client.on('message', msg => {
   if (msg.content === 'ping') {
-    msg.reply('Pong!');
+    var articles = scraper.articles
+    console.log(articles[0].title)
+    msg.reply(articles[0].title);
   }
 });
 
 client.login(config.TOKEN);
-
 
 module.exports = app
---
+++ utils/scraper.js
@@ -0,0 +1,33 @@
+const cheerio = require('cheerio')
+const request = require('request')
+
+//Site to be scraped
+const url = 'https://www.ampparit.com/uusimmat'
+
+function getArticles() {
+
+    //Array for the items
+    const articles = []
+
+    //Get title and header from the element 'news-item-headline'
+    request(url, (error, res, html) => {
+        if (!error && res.statusCode == 200) {
+            const $ = cheerio.load(html)
+            
+            //getting the headlines based on the className
+            $('.news-item-headline').each((i, el) => {
+                const title = $(el).text().trim()
+                const link = $(el).attr('href')
+
+                //Model
+                const item = {
+                    title: title,
+                    link: link,
+                }
+                articles.push(item)
+            })
+        }
+    })
+    return articles
+}
+module.exports = {articles: getArticles() }
