a82a249b440351ad4d60a4dddcf83173f07a1cc9
nyksy <71010049+nyksy@users.noreply.github.com>
2021-01-13 16:31:42 +0200

    added markdown to the articles to be sent

--- app.js
+++ app.js
@@ -15,9 +15,27 @@ client.on('ready', () => {
 
 client.on('message', msg => {
   if (msg.content === 'ping') {
+
+    //get articles from scraper.js
     var articles = scraper.articles
+    var date = new Date()
     console.log(articles[0].title)
-    msg.reply(articles[0].title);
+    msg.channel.send(
+      `**Aamujysäys ${Date()} by aivo**
+
+1. ${articles[0].title} <${articles[0].link}>
+
+2. ${articles[1].title} <${articles[1].link}>
+
+3. ${articles[2].title} <${articles[2].link}>
+
+4. ${articles[3].title} <${articles[3].link}>
+
+5. ${articles[4].title} <${articles[4].link}>
+
+
+*Kiitos näkemiin*`
+    )
   }
 });
 
